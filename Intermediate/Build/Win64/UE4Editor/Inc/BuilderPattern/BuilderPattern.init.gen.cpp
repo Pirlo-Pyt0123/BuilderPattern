ef75d9dad7f0a5962759f09b6a994afceed63a11
Pirlo-Pyt0123 <148081433+Pirlo-Pyt0123@users.noreply.github.com>
2025-04-12 11:04:36 -0400

    Enemy class go!

--- Intermediate/Build/Win64/UE4Editor/Inc/BuilderPattern/BuilderPattern.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/BuilderPattern/BuilderPattern.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBuilderPattern_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5B085B12,
-				0x2C0E98CE,
+				0xEC510312,
+				0x1063D329,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
